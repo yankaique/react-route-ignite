798def08e54b9ed519dfe968232dc876c827fcbf
Yan Marinho <yankaiquecosta.yk@gmail.com>
2021-03-29 07:23:35 -0300

    feature: import css file

--- reactjs/01-github-explorer/src/App.jsx
+++ reactjs/01-github-explorer/src/App.jsx
@@ -1,5 +1,7 @@
+import './styles/global.css';
+
 export function App(){
-    throw new Error('Deu erro meu parceiro');
+    // throw new Error('Deu erro meu parceiro');
 
     return <h1>Hello Yan</h1>
 }
