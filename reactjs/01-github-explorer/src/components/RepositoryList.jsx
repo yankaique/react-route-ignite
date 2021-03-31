ae055ae616aa00973ad2661013eb0f02b2953e51
Yan Marinho <yankaiquecosta.yk@gmail.com>
2021-03-31 07:18:31 -0300

    feature: list repos

--- reactjs/01-github-explorer/src/components/RepositoryList.jsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -2,20 +2,12 @@ import {useState, useEffect} from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 
-
-
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform'
-}
-
 export function RepositoryList(){
     const [repositories, setRepositories] = useState([]);
 
     useEffect(()=>{
         fetch('https://api.github.com/orgs/rocketseat/repos')
-        .then(response => response.jsonn())
+        .then(response => response.json())
         .then(data => setRepositories(data));
     },[]);
 
@@ -23,10 +15,10 @@ export function RepositoryList(){
         <section className="repository-list">
             <h1>Lista  de repositório</h1>
             <ul>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
+            {repositories.map(repository => {
+                return <RepositoryItem key={repository.name} repository={repository}/>
+            })
+            }
             </ul>
         </section>
     );
