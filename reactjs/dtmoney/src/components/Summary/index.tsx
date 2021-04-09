1165d0812e65ab899843de08a4ba5c6a507ff42c
Yan Marinho <yankaiquecosta.yk@gmail.com>
2021-04-09 08:01:31 -0300

    feature: create summary header

---
+++ reactjs/dtmoney/src/components/Summary/index.tsx
@@ -0,0 +1,39 @@
+import incomeIMG from '../../assets/icons/income.svg';
+import outcomeIMG from '../../assets/icons/outcome.svg';
+import totalIMG from '../../assets/icons/total.svg';
+
+import { Container } from './styles';
+
+export function Summary() {
+    return(
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={incomeIMG} alt="Entradas"/>
+                </header>
+                <strong>
+                    R$1000
+                </strong>
+            </div>
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={outcomeIMG} alt="Saídas"/>
+                </header>
+                <strong>
+                    - R$500
+                </strong>
+            </div>
+            <div className="highlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={totalIMG} alt="Total"/>
+                </header>
+                <strong>
+                    R$500
+                </strong>
+            </div>
+        </Container>
+    );
+}
