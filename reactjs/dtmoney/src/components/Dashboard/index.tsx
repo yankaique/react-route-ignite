d0b0f34ac9983a674ff63270ab115320f90b2f13
Yan Marinho <yankaiquecosta.yk@gmail.com>
2021-04-12 06:52:28 -0300

    feature: create transaction table

--- reactjs/dtmoney/src/components/Dashboard/index.tsx
+++ reactjs/dtmoney/src/components/Dashboard/index.tsx
@@ -1,10 +1,13 @@
 import { Summary } from '../Summary';
+import { TransactionsTable } from '../TransactionTable';
 import { Container } from './styles';
+
 
 export function Dashboard() {
     return(
         <Container>
-            <Summary />
+            <Summary />        
+            <TransactionsTable />
         </Container>
     );
 }
