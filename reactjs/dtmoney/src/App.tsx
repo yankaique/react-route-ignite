08d2d542f50df75e245f6f6f4bbaf852067c1d5d
Yan Marinho <yankaiquecosta.yk@gmail.com>
2021-04-13 07:19:44 -0300

    feature: create new transaction modal

--- reactjs/dtmoney/src/App.tsx
+++ reactjs/dtmoney/src/App.tsx
@@ -1,11 +1,31 @@
+import { useState } from 'react';
 import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 
+import Modal from 'react-modal';
+
 export function App() {
+  Modal.setAppElement('#root');
+
+  const [ isNewTransactionModalOpen, setIsNewTranactionModalOpen ] = useState(false);
+  
+  function handleOpenNewTransactionModal() {
+      setIsNewTranactionModalOpen(true);
+  }
+  
+  function handleCloseNewTransactionModal() {
+      setIsNewTranactionModalOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      <Modal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+          <h2>Cadastrar Transação</h2>
+      </Modal>
       <Dashboard />
       <GlobalStyle/>
     </>
